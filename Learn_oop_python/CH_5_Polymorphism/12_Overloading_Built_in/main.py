08b91fabb9b71dc40b34a5132168c36240e853e3
garvit <garvityadav07051999@gmail.com>
2024-07-27 00:47:27 +0530

    A: first commit

---
+++ Learn_oop_python/CH_5_Polymorphism/12_Overloading_Built_in/main.py
@@ -0,0 +1,8 @@
+
+class Dragon:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def __repr__(self):
+        return f"I am {self.name}, the {self.color} dragon"
