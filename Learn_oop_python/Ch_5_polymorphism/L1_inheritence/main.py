88dbfc3717ad34179fffef344953a6c24ced3716
garvityadav <garvit759@gmail.com>
2025-12-15 22:48:47 +0530

    completed CH1:L1 of Learn OOP python

---
+++ Learn_oop_python/Ch_5_polymorphism/L1_inheritence/main.py
@@ -0,0 +1,19 @@
+class Human:
+    def __init__(self, name):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+
+
+## don't touch above this line
+
+
+class Archer(Human):
+    def __init__(self, name, num_arrows):
+        super().__init__(name)
+        self.num_arrows = num_arrows
+
+    def get_num_arrows(self):
+        return self.num_arrows
+
