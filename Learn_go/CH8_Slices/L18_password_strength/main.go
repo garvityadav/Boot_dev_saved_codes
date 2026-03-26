5b49607f44c0a72af6d58e07a09e878415554f94
Garvityadav <garvityadav07051999@gmail.com>
2026-03-26 13:31:38 +0530

    completed L18 of CH8 => password strength

---
+++ Learn_go/CH8_Slices/L18_password_strength/main.go
@@ -0,0 +1,23 @@
+package main
+func isValidPassword(password string) bool {
+	if len(password) < 5 || len(password) >12 {
+		return false
+	}
+	hasUpper :=false
+	hasDigit :=false
+	
+	for _,char := range password{
+		if char >='A' && char <='Z'{
+			hasUpper = true
+		}
+		if char >='0' && char <='9'{
+			hasDigit = true
+		}
+	}
+	if hasUpper && hasDigit{
+		return true
+	}else{
+		return false
+	}
+}
+
